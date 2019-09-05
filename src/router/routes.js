41ff27339e173842abe7befdb95b3f8be7a42eb8
Fernando Moreira <nandomoreira.me@gmail.com>
2019-09-04 21:36:15 -0300

    add netlify

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,5 +8,10 @@ export default [
     path: '/about',
     name: 'about',
     component: () => import(/* webpackChunkName: "about" */ '@/views/about.vue')
+  },
+  {
+    path: '*',
+    name: 'error404',
+    component: () => import(/* webpackChunkName: "about" */ '@/views/404.vue')
   }
 ]
---
+++ src/views/404.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Error 404</h1>
+    <p>Oops, page not found!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'error404'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
